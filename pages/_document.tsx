76d3c29d2ed6aaec521ef89eef8a7fc8b2ea11a0
デンパ時計 <jsoldenpa@yahoo.co.jp>
2022-07-08 19:16:31 +0900

    fix: change link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,29 +9,85 @@ export default class MyDocument extends Document {
           {/* SEO stuff */}
           <title>Beat saber overlay generator</title>
 
-          <link rel="apple-touch-icon" sizes="57x57" href="/assets/apple-icon-57x57.png" />
-          <link rel="apple-touch-icon" sizes="60x60" href="/assets/apple-icon-60x60.png" />
-          <link rel="apple-touch-icon" sizes="72x72" href="/assets/apple-icon-72x72.png" />
-          <link rel="apple-touch-icon" sizes="76x76" href="/assets/apple-icon-76x76.png" />
-          <link rel="apple-touch-icon" sizes="114x114" href="/assets/apple-icon-114x114.png" />
-          <link rel="apple-touch-icon" sizes="120x120" href="/assets/apple-icon-120x120.png" />
-          <link rel="apple-touch-icon" sizes="144x144" href="/assets/apple-icon-144x144.png" />
-          <link rel="apple-touch-icon" sizes="152x152" href="/assets/apple-icon-152x152.png" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-icon-180x180.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="beat-saber-overlay-1/assets/apple-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="60x60"
+            href="beat-saber-overlay-1/assets/apple-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="beat-saber-overlay-1/assets/apple-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="beat-saber-overlay-1/assets/apple-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="beat-saber-overlay-1/assets/apple-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="beat-saber-overlay-1/assets/apple-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="beat-saber-overlay-1/assets/apple-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="beat-saber-overlay-1/assets/apple-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="beat-saber-overlay-1/assets/apple-icon-180x180.png"
+          />
           <link
             rel="icon"
             type="image/png"
             sizes="192x192"
-            href="/assets/android-icon-192x192.png"
+            href="beat-saber-overlay-1/assets/android-icon-192x192.png"
           />
-          <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="96x96" href="/assets/favicon-96x96.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="beat-saber-overlay-1/assets/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="96x96"
+            href="beat-saber-overlay-1/assets/favicon-96x96.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="beat-saber-overlay-1/assets/favicon-16x16.png"
+          />
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#ffffff" />
 
           {/* font loading */}
-          <link rel="preload" href="/fonts/NeonTubes2.otf" as="font" crossOrigin="" />
+          <link
+            rel="preload"
+            href="beat-saber-overlay-1/fonts/NeonTubes2.otf"
+            as="font"
+            crossOrigin=""
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link
