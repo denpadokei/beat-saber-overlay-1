dafded2e410db28067f22a5b4ee458eab8afd2f0
Morgolf <morgolef@gmail.com>
2022-07-08 18:09:25 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,7 @@ const Home = () => {
       <div className={classes.visualizers}>
         <div className={classes.wrapper}>
           <CutVisualizer
-            cellSize={68}
+            cellSize={70}
             fadeTime={50}
             gridColor="#fff"
             gridBorderSize={0}
@@ -54,7 +54,7 @@ const Home = () => {
             }}
           />
 
-          <div style={{ margin: -20 + (100 - (70 + (Math.SQRT2 - 1.08) * 70)) }} />
+          <div style={{ margin: -25 + (100 - (70 + (Math.SQRT2 - 1.08) * 70)) }} />
 
           <HitScoreVisualizer
             rows={1}
