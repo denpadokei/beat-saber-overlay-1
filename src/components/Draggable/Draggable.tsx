ff24695d79373d320ca76a378cb5d76415425f5f
デンパ時計 <jsoldenpa@yahoo.co.jp>
2022-07-08 16:01:05 +0900

    fix: e: any

--- src/components/Draggable/Draggable.tsx
+++ src/components/Draggable/Draggable.tsx
@@ -168,7 +168,7 @@ export const Draggable: FC<DraggableProps> = ({
     >
       <UnstyledButton
         ref={useMergedRef(boxRef, outsideClickRef, sizeRef)}
-        onClick={(e) => {
+        onClick={(e: any) => {
           if (!isOnOptionsNode(e)) {
             toggleOpened()
           }
