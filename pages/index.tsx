72ee0e33594295158b43d543b88286fc5599f783
Morgolf <morgolef@gmail.com>
2022-07-08 18:22:33 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,7 +54,7 @@ const Home = () => {
             }}
           />
 
-          <div style={{ margin: -35 + (100 - (70 + (Math.SQRT2 - 1.08) * 70)) }} />
+          <div style={{ margin: -30 + (100 - (70 + (Math.SQRT2 - 1.08) * 70)) }} />
 
           <HitScoreVisualizer
             rows={1}
