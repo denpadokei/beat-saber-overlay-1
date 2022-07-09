e9af95f4c3e7fff05c53aa3aa3bec726aabbf282
デンパ時計 <jsoldenpa@yahoo.co.jp>
2022-07-09 11:51:21 +0900

    fix: change manifest locale

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,7 +27,7 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="96x96" href="/assets/favicon-96x96.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png" />
-          <link rel="manifest" href="/manifest.json" />
+          <link rel="manifest" href="/assets/manifest.json" />
           <meta name="theme-color" content="#ffffff" />
 
           {/* font loading */}
